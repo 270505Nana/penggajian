d9c327f8b90561e8be7558c3bfe5594f1ade6a24
nana <prajna270505@gmail.com>
2022-05-12 07:50:26 +0700

    Update Edit Data, Delete Data Part 2

--- application/controllers/admin/data_pegawai.php
+++ application/controllers/admin/data_pegawai.php
@@ -89,7 +89,86 @@ class data_pegawai extends CI_Controller{
 
     // }
 
+    public function update_data($id){
+
+        $where  = array('id_pegawai' => $id);
+        $data['title'] = 'Update Data Pegawai';
+        $data['jabatan'] = $this->penggajian_models->get_data('data_jabatan')->result();
+        $data['pegawai'] = $this->db->query("SELECT * FROM data_pegawai WHERE id_pegawai='$id'")->result();
+       
+        $this->load->view('templates_admin/header',$data);
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/update_data_pegawai',$data);
+        $this->load->view('templates_admin/footer');
+    }
+
+   
+
+    public function update_data_aksi(){
+
+        // Untun mengirim data
+
+        // $this->_rules();
+
+        // if($this->form_validation->run() == FALSE){
+
+        //     $this->update_data();
+
+        // }else{
+            $id            = $this->input->post('id_pegawai');
+            $nik           = $this->input->post('nik');
+            $nama_pegawai  = $this->input->post('nama_pegawai');
+            $jenis_kelamin = $this->input->post('jenis_kelamin');
+            $jabatan       = $this->input->post('jabatan');
+            $tanggal_masuk = $this->input->post('tanggal_masuk');
+            $status        = $this->input->post('status');
+            // ditangkap
+            $data = array(
+                // disimpan
+                'nik'            => $nik,
+                'nama_pegawai'   => $nama_pegawai,
+                'jenis_kelamin'  => $jenis_kelamin,
+                'jabatan'        => $jabatan,
+                'tanggal_masuk'  => $tanggal_masuk,
+                'status'         => $status,
+                
+            );
+
+            $where =  array(
+                'id_pegawai' => $id
+            );
+
+            $this->penggajian_models->update_data('data_pegawai',$data,$where);
+            $this->session->set_flashdata('pesan','
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <strong>Data Berhasil di Ubah</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>');
+
+            redirect('admin/data_pegawai');
+        // } 
+    }
+
+    
+    public function delete_data($id){
+
+        $where = array('id_pegawai' => $id);
+        $this->penggajian_models->delete_data($where,'data_pegawai');
+
+        $this->session->set_flashdata('pesan','
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Data Berhasil di Hapus</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>');
+
+        redirect('admin/data_pegawai');
+
+    }
+
+
 }
-
-
 ?>
